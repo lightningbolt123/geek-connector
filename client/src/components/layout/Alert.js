4d6a8fb085efd53611cbdcbf58622ec8b4929e3a
lightningbolt123 <gabrielejenobo95@gmail.com>
2023-03-08 21:46:28 +0200

    I added top margin to the alert-container class

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && (
-    <div style={{ marginTop: '60px' }}>
+    <div className='alert-container'>
         {alerts.map( alert => (
             <div key={alert.id} className={`alert alert-${alert.alertType}`}>
                 {alert.msg}
